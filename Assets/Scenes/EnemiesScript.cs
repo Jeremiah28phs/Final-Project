ab0a4c7eb8ab17b0505469d5b9759947cfbd0880
JeremiahPhs <Jeremiah.Johnson28@Pallottihs.info>
2026-05-11 14:31:21 -0400

    Fireball

---
+++ Assets/Scenes/EnemiesScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemiesScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.position+= Vector2.left * Time.deltaTime;
+        //if(Input.GetButtonDown(KeyCode.e))
+        
+           // Shoot ();
+        
+    }
+    //void onTriggerEnter2D(Collider2D other)
+    
+       // if(other.gameObject.tag ("PlayerPower"))
+       // Destroy(gameObject);
+    
+   // void Shoot()
+    //rb.AddForce(firePoint.forward * lanchForce);
+    //
+}
---
+++ Assets/Scenes/ProjectileScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+
+{
+    public GameObject projectilePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.E))
+     {
+
+     
+      GameObject newProjectile= Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        Rigidbody2D projectileRB = newProjectile.GetComponent<Rigidbody2D>();
+        projectileRB.linearVelocity = Vector2.right;
+     } 
+    }
+}
